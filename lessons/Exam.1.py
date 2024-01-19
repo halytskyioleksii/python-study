7437e930ca353e46643be4af9ab033d704ed7afa
halytskyioleksii <halytskyioleksii@gmail.com>
2024-01-19 16:11:49 +0200

    added files

---
+++ lessons/Exam.1.py
@@ -0,0 +1,22 @@
+# 1 Завдання
+print("Привіт", "давай", "швидко", "всі", "гроші", sep="#", end="!!!")
+# 2 Завдання
+Number=input("\nВедіть сюди число: ")
+Number=int(Number)
+print("\n ви ввели число: ", Number)
+#1
+print("\n помножити на 7 =", Number * 7)
+#2
+print("\n відняти 3 =", Number - 3)
+#3
+print("\n в 5 степені", Number ** 5)
+# 3 Завдання
+Name=input("Введіть своє ім'я: ")
+birthYear=input("Введіть свій рік народження: ")
+Number=int(birthYear)
+print("Вам ", 2023 - Number, "років")
+# 4 завдання 
+Name=input("Введіть своє ім'я: ")
+Age=input("Скільки вам років?: ")
+birthYear=int(Age)
+print("Ваш рік народження ", 2023 - birthYear)
---
+++ lessons/Exam.2.py
@@ -0,0 +1,42 @@
+#1. Завдання
+a = int(input("Enter num 1: "))
+b = input("Enter num 2: ")
+
+a += 8
+print(a + int(b))
+
+#2. Завдання
+print("Мамо", "можна", "піти", "на", "вулицю", "з", "сестрою", sep=", ", end="***")
+print("\n")
+
+#3. Завдання
+Name=input("Введіть своє ім'я: ")
+Age=input("Скільки вам років?: ")
+Age=int(Age)
+print("Ваш рік народження ", 2023 - Age)
+
+#4. Завдання
+Number=input("\nВедіть сюди число: ")
+Number=int(Number)
+print("\n ви ввели число: ", Number)
+#1
+print("\n в 10 степені =", Number ** 10)
+#2
+print("\n помножити на 4 =", Number * 4)
+#3
+print("\n відняти 23", Number - 23)
+#4
+print("\n помножити на 2 =", Number * 2)
+#5
+print("\n додати 8 =", Number + 8)
+
+#5. Завдання
+Number=input("\nВедіть сюди число: ")
+Number=int(Number)
+print("\n ви ввели число: ", Number)
+#1
+print("\n додати 88 =", Number + 88)
+#2
+print("\n помножити на 9 =", Number * 9)
+
+myvariable = True #False 
